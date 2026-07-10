428879815e7d8ed09f433aeed8c20fdbaccae366
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-07-10 22:10:23 +0530

    feat (Customer): Created Customer Repository

--- Backend/src/main/java/com/SettleLater/Backend/customer/repository/CustomerRepository.java
+++ Backend/src/main/java/com/SettleLater/Backend/customer/repository/CustomerRepository.java
@@ -1,4 +1,21 @@
 package com.SettleLater.Backend.customer.repository;
 
-public class CustomerRepository {
+import com.SettleLater.Backend.customer.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findByShop_ShopId(String shopId);
+
+    List<Customer> findByShop_ShopIdAndCustomerName(String shopId, String customerName);
+
+    List<Customer> findByShop_ShopIdAndPhone(String shopId, String phone);
+
+    boolean existsByShop_ShopIdAndPhone(String shopId, String phone);
+
+    Page<Customer> findByShop_ShopIdAndIsActiveTrue(String shopId, Pageable pageable);
 }
